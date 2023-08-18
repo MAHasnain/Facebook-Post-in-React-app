4d8742240a888a299b3cd300109fa8285f5e9cd2
M A Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-08-18 22:41:48 +0500

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 // import './index.css';
-import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <App name = {"MAHasnain"} date = {"3d"}/>
-  <App name = {"M.A.H"} date = {"1d"} /> 
-  <App name = {"Hasnain"} date = {"4h"} />
+    <App
+      topText={"Suggested for you"}
+      threeDotsImg ={"./img/threeDots.jpg"}
+      crossImg ={"./img/cross.png"}
+      profileImg={"./img/profilePic.jpg"}
+      name={"M A Hasnain"}
+      date={"3d"}
+      img={"./img/postImg.jpg"}
+      text={"#cityview #travel #birdeyeview"}
+      cmts={"10 comments 5 shares"}
+      FBreaction={"./img/shocked.png"}
+      FBreaction2={"./img/lovely-heart.png"}
+      reactionQty={"You and 17"}
+    />
+    <App
+      text2={
+        "Imposing mountains kiss the sky, their rugged peaks a testament to nature's grandeur."
+      }
+      threeDotsImg ={"./img/threeDots.jpg"}
+      crossImg ={"./img/cross.png"}
+      topText={"Suggested for you"}
+      profileImg={"./img/profilePic2.png"}
+      name={"Zimal Khan"}
+      date={"1d"}
+      img={"./img/postImg2.jpg"}
+      cmts={"17 comments  11 shares"}
+      FBreaction={"./img/thumb.png"}
+      FBreaction2={"./img/lovely-heart.png"}
+      reactionQty={"11"}
+    />
+    <App
+      text2={
+        "Silent sentinels beneath the starlit canopy, mountains unveil a majestic nocturnal tapestry."
+      }
+      threeDotsImg ={"./img/threeDots.jpg"}
+      crossImg ={"./img/cross.png"}
+      topText={"Suggested for you"}
+      profileImg={"./img/profilePic3.png"}
+      name={"M Asad Ali"}
+      date={"4h"}
+      img={"./img/postImg3.jpg"}
+      text={"#NightVibes #NightSummits #StarryNight"}
+      cmts={"14 comments 13 shares"}
+      FBreaction={"./img/lovely-heart.png"}
+      FBreaction2={"./img/thumb.png"}
+      reactionQty={"You and 17"}
+    />
+    {/* <App
+      text2={
+        "Silent sentinels beneath the starlit canopy, mountains unveil a majestic nocturnal tapestry."
+      }
+      threeDotsImg ={"./img/threeDots.jpg"}
+      crossImg ={"./img/cross.png"}
+      topText={"Suggested for you"}
+      profileImg={"./img/profilePic3.png"}
+      name={"M Asad Ali"}
+      date={"4h"}
+      img={"./img/postImg3.jpg"}
+      text={"#NightVibes #NightSummits #StarryNight"}
+      cmts={"14 comments 13 shares"}
+      FBreaction={"./img/lovely-heart.png"}
+      FBreaction2={"./img/thumb.png"}
+      reactionQty={"You and 17"}
+    /> */}
   </React.StrictMode>
 );
 
